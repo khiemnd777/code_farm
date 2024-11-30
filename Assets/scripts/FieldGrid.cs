6462c74a8c6c8e3eeb7dd3c1a9e6130acce3cf3a
khiem.nguyen <knguyen20@wellbarrier.slb.com>
2024-11-30 12:20:20 +0700

    performance

--- Assets/scripts/FieldGrid.cs
+++ Assets/scripts/FieldGrid.cs
@@ -1,9 +1,11 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FieldGrid : MonoBehaviour
 {
     public List<FieldTile> fieldTiles = new();
+
     [SerializeField]
     ObstacleSegmentController _obstacleSegmentController;
 
@@ -47,21 +49,31 @@ public class FieldGrid : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GenerateGrid();
+        StartCoroutine(GenerateGrid());
     }
 
-    void GenerateGrid()
+    IEnumerator GenerateGrid()
     {
         for (int x = 0; x < _initialWidth; x++)
         {
-            var fieldTickH = Instantiate<FieldTick>(_tickHPrefab, new Vector3(x, 1), Quaternion.identity, this.transform);
+            var fieldTickH = Instantiate<FieldTick>(
+                _tickHPrefab,
+                new Vector3(x, 1),
+                Quaternion.identity,
+                this.transform
+            );
             fieldTickH.tick = x;
 
             for (int y = 0; y < _initialHeight; y++)
             {
                 if (x == 0)
                 {
-                    var fieldTickV = Instantiate<FieldTick>(_tickVPrefab, new Vector3(-1, -y), Quaternion.identity, this.transform);
+                    var fieldTickV = Instantiate<FieldTick>(
+                        _tickVPrefab,
+                        new Vector3(-1, -y),
+                        Quaternion.identity,
+                        this.transform
+                    );
                     fieldTickV.tick = y;
                 }
 
@@ -69,25 +81,41 @@ public class FieldGrid : MonoBehaviour
 
                 if (x == _initialWidth - 1 && y < _initialHeight - 1)
                 {
-                    fieldTile = Instantiate<FieldTile>(_fieldTileLastHPrefab, new Vector3(x, -y), Quaternion.identity, this.transform);
+                    fieldTile = Instantiate<FieldTile>(
+                        _fieldTileLastHPrefab,
+                        new Vector3(x, -y),
+                        Quaternion.identity,
+                        this.transform
+                    );
                 }
                 else if (x < _initialWidth - 1 && y == _initialHeight - 1)
                 {
-                    fieldTile = Instantiate<FieldTile>(_fieldTileLastVPrefab, new Vector3(x, -y), Quaternion.identity, this.transform);
+                    fieldTile = Instantiate<FieldTile>(
+                        _fieldTileLastVPrefab,
+                        new Vector3(x, -y),
+                        Quaternion.identity,
+                        this.transform
+                    );
                 }
                 else if (x == _initialWidth - 1 && y == _initialHeight - 1)
                 {
-                    fieldTile = Instantiate<FieldTile>(_fieldTileLastPrefab, new Vector3(x, -y), Quaternion.identity, this.transform);
+                    fieldTile = Instantiate<FieldTile>(
+                        _fieldTileLastPrefab,
+                        new Vector3(x, -y),
+                        Quaternion.identity,
+                        this.transform
+                    );
                 }
                 else
                 {
-                    fieldTile = Instantiate<FieldTile>(_fieldTilePrefab, new Vector3(x, -y), Quaternion.identity, this.transform);
+                    fieldTile = Instantiate<FieldTile>(
+                        _fieldTilePrefab,
+                        new Vector3(x, -y),
+                        Quaternion.identity,
+                        this.transform
+                    );
                 }
-                fieldTile.field = new Field
-                {
-                    x = x,
-                    y = y,
-                };
+                fieldTile.field = new Field { x = x, y = y, };
                 if (_obstacleSegmentController)
                 {
                     foreach (var edgeHighlighter in fieldTile.edgeHighlighters)
@@ -101,6 +129,7 @@ public class FieldGrid : MonoBehaviour
                 }
                 fieldTiles.Add(fieldTile);
             }
+            yield return null;
         }
     }
 }
